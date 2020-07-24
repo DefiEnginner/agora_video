9a90488fa1b39a7fdeb95a2c0479aa423685c864
jihandodi <jihandodi0909@gmail.com>
2020-07-24 12:56:49 -0400

    browser check separate component

---
+++ src/components/BrowserCheck/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { notification } from "antd";
+import {
+  isChrome,
+  isFirefox,
+  isIOS,
+  isSafari,
+  isAndroid,
+  isIE,
+  isEdge,
+} from "react-device-detect";
+import { withRouter } from "react-router-dom";
+
+const BrowserCheck = ({ redirectURL = "/", children, history }) => {
+  const iosAbort = isIOS && (isChrome || isFirefox);
+  const androidAbort = isAndroid && (isSafari || isChrome);
+  const edgeAbort = isIE || isEdge;
+
+  const errMessage = iosAbort
+    ? "We only support safari on iPhone. Please check your browser and join again."
+    : androidAbort
+    ? "We only support Chrome on Android devices. Please check your browser and join again."
+    : "We do not support IE and Edge Browsers. Please check your browser and join again.";
+
+  if (iosAbort || androidAbort || edgeAbort) {
+    history.push(redirectURL);
+    notification.error({
+      message: "Browser not supported",
+      description: errMessage,
+    });
+
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default withRouter(BrowserCheck);
--- src/pages/VideoCallPage/index.js
+++ src/pages/VideoCallPage/index.js
@@ -1,44 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
-import { Spin, notification } from "antd";
-import { isChrome, isFirefox, isIOS } from "react-device-detect";
+import { Spin } from "antd";
 
 import AgoraVideo from "../../components/AgoraVideo";
 import DrawerControls from "../../components/DrawerControls";
+import BrowserCheck from "../../components/BrowserCheck";
 import "./index.css";
 
 const VideoCallPage = (props) => {
   const [remoteJoined, setRemoteJoined] = useState(false);
-  const shouldAbort = isIOS && (isChrome || isFirefox);
-
-  useEffect(() => {
-    if (shouldAbort) {
-      props.history.push("/");
-      notification.error({
-        message: "Browser not supported",
-        description:
-          "We only support safari on iPhone. Please check your browser and join again.",
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const leaveCall = () => props.history.push("/");
 
-  return shouldAbort ? null : (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <AgoraVideo
-        remoteJoined={remoteJoined}
-        setRemoteJoined={setRemoteJoined}
-        leaveCall={leaveCall}
-      />
-      {!remoteJoined && (
-        <div className="loader">
-          <Spin /> Waiting For Participant ...
-        </div>
-      )}
-      {remoteJoined && <DrawerControls />}
-    </div>
+  return (
+    <BrowserCheck>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <AgoraVideo
+          remoteJoined={remoteJoined}
+          setRemoteJoined={setRemoteJoined}
+          leaveCall={leaveCall}
+        />
+        {!remoteJoined && (
+          <div className="loader">
+            <Spin /> Waiting For Participant ...
+          </div>
+        )}
+        {remoteJoined && <DrawerControls />}
+      </div>
+    </BrowserCheck>
   );
 };
 
